60cf1dd4e7308ae10375f29a869a38c8c4c93d95
Calvin Huang <c@lvin.me>
2016-10-18 14:50:59 -0400

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'scaleapi',
     packages = ['scaleapi'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'The official Python client library for the Scale API, the API for human labor.',
     author = 'Calvin Huang',
     author_email = 'c@lvin.me',
