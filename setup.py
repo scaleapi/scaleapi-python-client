7b24e9238d54487d0fa3227ef8e872ae8b5cf90d
Calvin Huang <c@lvin.me>
2016-10-17 23:02:33 -0400

    version bump, add keywords/classifiers

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from distutils.core import setup
 setup(
     name = 'scaleapi',
     packages = ['scaleapi'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'A client library for interacting with the Scale API',
     author = 'Calvin Huang',
     author_email = 'c@lvin.me',
     url = 'https://github.com/scaleapi/scale-api-python-client',
     download_url = 'https://github.com/scaleapi/scale-api-python-client/tarball/0.1',
-    keywords = ['scale', 'scaleapi'],
+    keywords = ['scale', 'scaleapi', 'humans', 'tasks', 'categorization', 'transcription', 'annotation', 'comparison', 'data collection', 'phone call'],
     install_requires = ['requests', 'enum34'],
-    classifiers = [],
+    classifiers = ['Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'License :: OSI Approved :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries']
 )
