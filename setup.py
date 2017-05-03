6f03dbc1428567108b6c33b443d0af0f4df7adc6
Alexandr Wang <avatar09@gmail.com>
2017-05-02 19:54:12 -0700

    Hotfix, fix SNI issues with <python 2.7.9

--- setup.py
+++ setup.py
@@ -1,15 +1,38 @@
-from distutils.core import setup
+import sys
+import warnings
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install_requires = ['requests>=2.4.2', 'enum34']
+
+if sys.version_info < (2, 7, 9):
+    warnings.warn(
+        'Users have reported issues with SNI / SSL by using Scale on '
+        'versions of Python older than 2.7.9. If at all possible, you should '
+        'upgrade your version of Python. '
+        'If you have any questions, please file an issue on Github or '
+        'contact us at support@scaleapi.com.',
+        DeprecationWarning)
+    install_requires.append('pyOpenSSL')
+    install_requires.append('ndg-httpsclient')
+    install_requires.append('pyasn1')
+    install_requires.append('idna')
+    install_requires.append('requests[security]')
+
 setup(
     name = 'scaleapi',
     packages = ['scaleapi'],
-    version = '0.1.7',
+    version = '0.1.8',
     description = 'The official Python client library for the Scale API, the API for human labor.',
     author = 'Calvin Huang',
     author_email = 'c@lvin.me',
     url = 'https://github.com/scaleapi/scaleapi-python-client',
     download_url = 'https://github.com/scaleapi/scaleapi-python-client/tarball/0.1.2',
     keywords = ['scale', 'scaleapi', 'humans', 'tasks', 'categorization', 'transcription', 'annotation', 'comparison', 'data collection', 'phone call', 'audio transcription'],
-    install_requires = ['requests>=2.4.2', 'enum34'],
+    install_requires = install_requires,
     classifiers = ['Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.5',
                    'License :: OSI Approved :: MIT License',
