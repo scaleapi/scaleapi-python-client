139e5212e9bc250591abf86d6c9e778fe7f97e0d
Shaun VanWeelden <shaun.vanweelden@scaleapi.com>
2021-03-22 12:01:18 -0500

    Add more supported versions to PyPI

--- setup.py
+++ setup.py
@@ -49,17 +49,19 @@ setup(
     keywords=[
         'scale',
         'scaleapi',
-        'humans',
         'tasks',
         'categorization',
-        'transcription',
+        'labeling',
         'annotation',
-        'data collection',
-        'audio transcription'
     ],
     install_requires=install_requires,
     classifiers=['Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
                  'License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers',
                  'Topic :: Software Development :: Libraries']
