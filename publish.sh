65f216cdc320484975741ab2f55a7d324dbde9be
Fatih Kurtoglu <fatih.kurtoglu@scale.com>
2021-03-31 22:27:15 -0700

    automated publish to pypi

---
+++ publish.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo "##### STARTING BUILD and PUBLISH #####"
+echo
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${DIR}" || exit 1
+
+BRANCH_NAME=$(git branch 2>/dev/null | grep '^*' | tr -d ' *')
+echo "Active Git Branch: ${BRANCH_NAME}" # release-1.0.5
+
+# IFS='-' read -ra strarr <<< "$BRANCH_NAME"
+# BRANCH_PREFIX="${strarr[0]}"  # release
+# BRANCH_VERSION="${strarr[1]}" # 1.0.5
+
+while IFS= read -r line; do
+    if [[ $line == __version__* ]];
+    then
+        IFS=' = ' read -ra strarr <<< "$line"
+        PKG_VERSION=$( sed -e 's/^"//' -e 's/"$//' <<< "${strarr[1]}" )
+        echo "SDK Package Version: ${PKG_VERSION}"
+        break
+    fi
+done < "${DIR}/scaleapi/_version.py"
+
+if [ "$BRANCH_NAME" != "release-${PKG_VERSION}" ];
+then
+    echo "ERROR: You need to be in 'release-${PKG_VERSION}' git branch to publish this version (${PKG_VERSION})."
+    exit 1
+fi
+
+if [ "$1" == "runtest" ];
+then
+    echo "Validating environment variable [SCALE_TEST_API_KEY] for pytest..."
+    if [[ -z "${SCALE_TEST_API_KEY}" ]]; then
+        echo "Test key not found. Please assign 'SCALE_TEST_API_KEY=...' as your test environment key."
+        exit 1
+    fi
+
+
+    if ! python -m pytest; then echo "ERROR: pytest failed."; exit; fi
+    echo "pytest is successful!"
+fi
+
+# Clean-up build and dist folders
+rm -rf build/ dist/
+
+# Build package
+echo "Building package..."
+
+if ! python3 setup.py sdist bdist_wheel > /dev/null 2>&1; then echo "ERROR: Package building failed."; exit 1; fi
+
+
+if ! pip show twine > /dev/null 2>&1;
+then
+    echo "WARN: 'twine' package is not found, installing...";
+    pip install twine
+fi
+
+# Twine Validation
+echo "Validating package..."
+
+if ! twine check --strict dist/* ; then echo "ERROR: Twine check failed."; exit 1; fi
+
+# Twine Upload to Pypi
+echo "Uploading package..."
+
+if ! twine upload dist/*; then echo "ERROR: Twine upload failed."; exit 1; fi
+
+exit 0;
