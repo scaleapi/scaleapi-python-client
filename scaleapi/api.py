51c18a3ca39d7750b2eeaa17fb49cd79856d409c
Jonathan Feng <jon.feng@scale.com>
2022-10-17 19:30:25 -0700

    Update api.py

--- scaleapi/api.py
+++ scaleapi/api.py
@@ -115,12 +115,14 @@ class Api:
             # RequestHistory(method='POST', url='/v1/task/imageannotation', error=None, status=409, redirect_location=None)
             if retry_history != ():
                 # See if the first retry was a 500 error
-                if retry_history[0][3] == 500: # Changed to 409 to easily test by running test_unique_id_fail
-                    uuid = body['unique_id']
-                    newUrl = f'https://api.scale.com/v1/tasks?unique_id={uuid}'
+                if retry_history[0][3] == 500:
+                    uuid = body["unique_id"]
+                    newUrl = f"https://api.scale.com/v1/tasks?unique_id={uuid}"
                     # grab task response via uuid by hitting /tasks endpoint
-                    newRes = self._http_request("GET", newUrl, headers=headers, auth=auth)
-                    json = newRes.json()['docs'][0]
+                    newRes = self._http_request(
+                        "GET", newUrl, headers=headers, auth=auth
+                    )
+                    json = newRes.json()["docs"][0]
             else:
                 self._raise_on_respose(res)
         else:
